c3309b89ce837bca9cfc44510fbdf5ee86e67e51
kslkhailian-mango <kslkhailian@gmail.com>
2026-05-31 15:01:36 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -50,6 +50,8 @@ io.on("connection", (socket) => {
     console.log("User connected");
 });
 
-http.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
