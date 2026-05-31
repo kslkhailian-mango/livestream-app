82e166832f4fe700552c528f57fb6dbee6957dc5
kslkhailian-mango <kslkhailian@gmail.com>
2026-05-31 17:43:21 -0600

    Update server.js

--- server.js
+++ server.js
@@ -16,14 +16,12 @@ app.use(session({
 app.use(express.static("public"));
 
 let users = [];
+let broadcaster = null;
 
 app.post("/register", (req, res) => {
     const { username, password } = req.body;
 
-    users.push({
-        username,
-        password
-    });
+    users.push({ username, password });
 
     res.json({
         success: true,
@@ -38,20 +36,44 @@ app.post("/login", (req, res) => {
         u => u.username === username && u.password === password
     );
 
-    if(user){
+    if (user) {
         req.session.user = user;
-        res.json({ success:true });
+        res.json({ success: true });
     } else {
-        res.json({ success:false });
+        res.json({ success: false });
     }
 });
 
 let viewerCount = 0;
 
 io.on("connection", (socket) => {
-    viewerCount++;
+    console.log("User connected:", socket.id);
 
+    viewerCount++;
     io.emit("viewer-count", viewerCount);
+
+    socket.on("broadcaster", () => {
+        broadcaster = socket.id;
+        socket.broadcast.emit("broadcaster");
+    });
+
+    socket.on("watcher", () => {
+        if (broadcaster) {
+            io.to(broadcaster).emit("watcher", socket.id);
+        }
+    });
+
+    socket.on("offer", (id, message) => {
+        io.to(id).emit("offer", socket.id, message);
+    });
+
+    socket.on("answer", (id, message) => {
+        io.to(id).emit("answer", socket.id, message);
+    });
+
+    socket.on("candidate", (id, message) => {
+        io.to(id).emit("candidate", socket.id, message);
+    });
 
     socket.on("chat-message", (data) => {
         io.emit("chat-message", data);
@@ -60,8 +82,16 @@ io.on("connection", (socket) => {
     socket.on("disconnect", () => {
         viewerCount--;
         io.emit("viewer-count", viewerCount);
+
+        socket.to(broadcaster).emit("disconnectPeer", socket.id);
+
+        if (socket.id === broadcaster) {
+            broadcaster = null;
+            socket.broadcast.emit("broadcaster-disconnected");
+        }
     });
 });
+
 const PORT = process.env.PORT || 3000;
 
 http.listen(PORT, () => {
