1ac9e9f6838ea520c21bb5d71db1e883c5d07549
kslkhailian-mango <kslkhailian@gmail.com>
2026-06-01 06:40:31 -0600

    Penglam live UI completed

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
 const express = require("express");
 const session = require("express-session");
+
+mongoose.connect("mongodb+srv://PenglamFoundation:Zammun%40123@cluster0.d3tgnwk.mongodb.net/test?retryWrites=true&w=majority")
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
 
 const app = express();
 const http = require("http").createServer(app);
@@ -18,14 +19,40 @@ app.use(session({
 }));
 
 app.use(express.static("public"));
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/home.html");
+});
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String
+    username: String,
+    password: String,
+
+    avatar: {
+        type: String,
+        default: "/MMGPENGLAM1.png"
+    },
+
+    followers: {
+        type: Number,
+        default: 0
+    },
+
+    following: {
+        type: Number,
+        default: 0
+    },
+
+    coins: {
+        type: Number,
+        default: 0
+    },
+
+    bio: {
+        type: String,
+        default: ""
+    }
 });
 
 const User = mongoose.model("User", userSchema);
-
-let broadcaster = null;
 
 app.post("/register", async (req, res) => {
     const { username, password } = req.body;
@@ -58,23 +85,109 @@ app.post("/login", async (req, res) => {
         res.json({ success: false });
     }
 });
+app.get("/profile/:username", async (req, res) => {
 
-let viewerCount = 0;
+    const user = await User.findOne({
+        username: req.params.username
+    });
 
+    if (!user) {
+        return res.json({
+            success: false
+        });
+    }
+
+    res.json({
+        success: true,
+        user
+    });
+
+});
+async function followHandler(req, res) {
+    const user = await User.findOne({
+        username: req.params.username
+    });
+
+    if (!user) {
+        return res.json({ success: false });
+    }
+
+    user.followers += 1;
+    await user.save();
+
+    res.json({
+        success: true,
+        followers: user.followers
+    });
+}
+app.post("/unfollow/:username", async (req, res) => {
+
+    const user = await User.findOne({
+        username: req.params.username
+    });
+
+    if (!user) {
+        return res.json({ success: false });
+    }
+
+    if (user.followers > 0) {
+        user.followers -= 1;
+    }
+
+    await user.save();
+
+    res.json({
+        success: true,
+        followers: user.followers
+    });
+});
+app.get("/follow/:username", followHandler);
+app.post("/follow/:username", followHandler);
+
+const broadcasters = {};
+const viewerCounts = {};
+const liveRooms = {};
+app.get("/live-rooms", (req, res) => {
+    const rooms = Object.values(liveRooms).map(room => ({
+        ...room,
+        viewers: viewerCounts[room.roomId] || 0
+    }));
+
+    res.json(rooms);
+});
 io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
 
-    viewerCount++;
-    io.emit("viewer-count", viewerCount);
+    socket.on("join-room", (roomId) => {
+        socket.join(roomId);
 
-    socket.on("broadcaster", () => {
-        broadcaster = socket.id;
-        socket.broadcast.emit("broadcaster");
+        if (!viewerCounts[roomId]) {
+            viewerCounts[roomId] = 0;
+        }
+
+        viewerCounts[roomId]++;
+        io.to(roomId).emit("viewer-count", viewerCounts[roomId]);
     });
 
-    socket.on("watcher", () => {
-        if (broadcaster) {
-            io.to(broadcaster).emit("watcher", socket.id);
+socket.on("broadcaster", (roomId) => {
+    broadcasters[roomId] = socket.id;
+    socket.join(roomId);
+
+    liveRooms[roomId] = {
+        roomId,
+        title: roomId,
+        startedAt: new Date()
+    };
+
+    socket.to(roomId).emit("broadcaster");
+    io.emit("live-rooms-updated");
+});
+
+    socket.on("watcher", (roomId) => {
+        socket.join(roomId);
+
+        if (broadcasters[roomId]) {
+            io.to(broadcasters[roomId]).emit("watcher", socket.id);
         }
     });
 
@@ -91,20 +204,33 @@ io.on("connection", (socket) => {
     });
 
     socket.on("chat-message", (data) => {
-        io.emit("chat-message", data);
+        io.to(data.roomId).emit("chat-message", data);
     });
 
     socket.on("disconnect", () => {
-        viewerCount--;
-        io.emit("viewer-count", viewerCount);
+        for (const roomId of socket.rooms) {
+            if (roomId !== socket.id && viewerCounts[roomId]) {
+                viewerCounts[roomId]--;
+                io.to(roomId).emit("viewer-count", viewerCounts[roomId]);
+            }
 
-        socket.to(broadcaster).emit("disconnectPeer", socket.id);
-
-        if (socket.id === broadcaster) {
-            broadcaster = null;
-            socket.broadcast.emit("broadcaster-disconnected");
+            if (broadcasters[roomId] === socket.id) {
+    delete broadcasters[roomId];
+    delete liveRooms[roomId];
+    socket.to(roomId).emit("broadcaster-disconnected");
+    io.emit("live-rooms-updated");
+}
         }
+
+        for (const roomId in broadcasters) {
+            io.to(broadcasters[roomId]).emit("disconnectPeer", socket.id);
+        }
+
+        console.log("User disconnected:", socket.id);
     });
+socket.on("chat-message", data => {
+    io.emit("chat-message", data);
+});    
 });
 
 const PORT = process.env.PORT || 3000;
