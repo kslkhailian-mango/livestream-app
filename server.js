9eda28bc9c6fda250d66d4ad9db0ce8f916bf91b
kslkhailian-mango <kslkhailian@gmail.com>
2026-06-03 20:52:40 -0600

    fix upload folder error

--- server.js
+++ server.js
@@ -55,10 +55,12 @@ const userSchema = new mongoose.Schema({
 app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
 
 const storage = multer.diskStorage({
-  destination: "public/uploads",
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  }
+    destination: (req, file, cb) => {
+        cb(null, "public/uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "-" + file.originalname);
+    }
 });
 
 const upload = multer({ storage });
